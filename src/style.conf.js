d99bc1ce2b33d34d4e7c23d9fac7d0cd088eb7d4
Maxim Dementyev <max@orofarne.ru>
2017-11-16 09:14:53 +0300

    map-coffee style

--- src/style.conf.js
+++ src/style.conf.js
@@ -1,5 +1,6 @@
 //	import glStyle from './res/standard/style.json'
 // import glStyle from './res/sputnik2g1.json'
 // import glStyle from './res/lite3-v9ru.json'
-import glStyle from './res/bright-v9ru.json'
+// import glStyle from './res/bright-v9ru.json'
+import glStyle from './res/map-coffee.json'
 export default glStyle;
